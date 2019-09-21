ae3228feb01e925430ca92b58c6f26298d77bf1a
Nameless <truemedian@gmail.com>
2019-09-21 14:08:43 -0500

    create lit package for easy install

---
+++ package.lua
@@ -0,0 +1,17 @@
+  return {
+    name = "truemedian/musicord",
+    version = "1.0.0",
+    description = "A port of xieve/musicord for discordia 2.x",
+    tags = { "lua", "discord" },
+    license = "Unlicense",
+    author = { name = "Nameless", email = "truemedian@gmail.com" },
+    homepage = "https://github.com/truemedian/musicord",
+    dependencies = {
+        "SinisterRectus/discordia@2.6.0",
+        "creationix/coro-split@2.0.0",
+    },
+    files = {
+        "**.lua",
+    }
+}
+  
